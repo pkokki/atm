2cdf595e283cb59d94cf37d530f644fb0fba1900
Panos <pkokki@gmail.com>
2015-04-02 18:17:04 +0300

    added protractor

---
+++ tests/09_identifying_a_customer.js
@@ -0,0 +1,23 @@
+﻿describe('09 Identifying A Customer', function() {
+	// var firstNumber = element(by.model('first'));
+	
+	beforeEach(function() {
+		browser.get('http://localhost:3000/#/atm');
+	});
+	
+	it('09.05 CUSTOMER CARD VERIFY', function() {
+		// Typical successful transaction
+		
+		// It is assumed that the Customer Card Verify transaction is automatically initiated when the
+		// customer enters the card in the ATM.
+		
+	});
+	
+	it('09.06 ACCOUNT VERIFY AND OAR', function() {
+		// Successful retrieval of a list of accounts which are linked to a card.
+	});
+	
+	it('09.07 CARD CAPTURE', function() {
+		// Customer walk away after a time out when attempting to return the card
+	});
+});
---
+++ tests/10_withdrawal_transactions.js
@@ -0,0 +1,71 @@
+﻿describe('10 Withdrawal Transactions', function() {
+	beforeEach(function() {
+		browser.get('http://localhost:3000/#/atm/withdrawal');
+	});
+	
+	it('10.04 Withdrawal', function() {
+		// This scenario illustrates a typical successful withdrawal transaction.
+		
+		// [1]  The customer selects a withdrawal from a deposit 
+		// 		account (savings or checking) and agrees to the
+		//		associated surcharge fee. Any messages involved with
+		//		card type and fee amount determination is out of band.
+		element(by.buttonText("Other")).click();
+		
+		element(by.model('amount')).sendKeys('120');
+		//element(by.css('[ng-click="requestWithdrawal(amount)"]')).click();
+		element(by.buttonText('Continue')).click();
+		
+		// http://localhost:3000/#/atm/dispenseCash
+		expect(browser.getCurrentUrl()).toContain('#/atm/dispenseCash');
+		element(by.css('[ng-click="takeMoney()"]')).click();
+		
+		// http://localhost:3000/#/atm/continue
+		expect(browser.getCurrentUrl()).toContain('#/atm/continue');
+	});
+	/*
+	it('10.05 Favorite Withdrawal', function() {
+	
+	});
+	
+	it('10.08 Withdrawal, Acquiring Server (CSP) determines surcharge, customer declines', function() {
+	
+	});
+	
+	it('10.09 Withdrawal, Issuing Server determines surcharge', function() {
+	
+	});
+	
+	it('10.11 Withdrawal, Issuing Server determines surcharge, customer declines', function() {
+	
+	});
+	
+	it('10.11 Withdrawal, device fault', function() {
+	
+	});
+	
+	it('10.12 Withdrawal, shutter fault', function() {
+	
+	});
+	
+	it('10.13 Withdrawal, communication exception', function() {
+	
+	});
+	
+	it('10.14 Withdrawal, partial dispense', function() {
+	
+	});
+	
+	it('10.16 Reversal Scenario Summary', function() {
+	
+	});
+	
+	it('10.17 Withdrawal Denied Due to “No Sharing Arrangement”', function() {
+	
+	});
+	
+	it('10.18 Successful Track Update', function() {
+	
+	});
+	*/
+})
